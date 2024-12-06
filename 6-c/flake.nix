d7b345f90326887b0d040ac6e07c2e12d413282a
arlohb <arlo@urwick.co.uk>
2024-12-06 15:15:15 +0000

    Day 6 part 1

---
+++ 6-c/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A simple c project with gcc and make";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            clang-tools
+            bear
+          ];
+        };
+      }
+    );
+}
