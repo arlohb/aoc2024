dbf88e0846c05d47272df96ee6c1b1d69e6113e8
arlohb <arlo@urwick.co.uk>
2024-12-14 12:40:53 +0000

    Day 14 part 1

---
+++ 14-rust/src/main.rs
@@ -0,0 +1,162 @@
+use std::{
+    ops::{Add, AddAssign},
+    str::FromStr,
+};
+
+use anyhow::anyhow;
+
+#[allow(dead_code)]
+const EXAMPLE_WIDTH: u32 = 11;
+#[allow(dead_code)]
+const EXAMPLE_HEIGHT: u32 = 7;
+
+#[allow(dead_code)]
+const REAL_WIDTH: u32 = 101;
+#[allow(dead_code)]
+const REAL_HEIGHT: u32 = 103;
+
+const WIDTH: u32 = REAL_WIDTH;
+const HEIGHT: u32 = REAL_HEIGHT;
+
+/// Starts at top left
+#[derive(Debug, Clone, Copy)]
+pub struct Vector2 {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl FromStr for Vector2 {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (_, s) = s.split_once("=").ok_or(anyhow!("Invalid input"))?;
+        let (x, y) = s.split_once(",").ok_or(anyhow!("Invalid input"))?;
+
+        Ok(Self::new(x.parse()?, y.parse()?))
+    }
+}
+
+impl Add for Vector2 {
+    type Output = Vector2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector2::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl AddAssign for Vector2 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Vector2 {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug)]
+pub struct Robot {
+    pub pos: Vector2,
+    pub vel: Vector2,
+}
+
+impl FromStr for Robot {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (p, v) = s.split_once(" ").ok_or(anyhow!("Invalid input"))?;
+
+        let p: Vector2 = p.parse()?;
+        let v: Vector2 = v.parse()?;
+
+        Ok(Robot { pos: p, vel: v })
+    }
+}
+
+impl Robot {
+    pub fn advance(&mut self) {
+        self.pos += self.vel;
+
+        if self.pos.x < 0 {
+            self.pos.x += WIDTH as i32;
+        }
+
+        if self.pos.x >= WIDTH as i32 {
+            self.pos.x -= WIDTH as i32;
+        }
+
+        if self.pos.y < 0 {
+            self.pos.y += HEIGHT as i32;
+        }
+
+        if self.pos.y >= HEIGHT as i32 {
+            self.pos.y -= HEIGHT as i32;
+        }
+    }
+
+    pub fn advance_n(&mut self, n: u32) {
+        for _ in 0..n {
+            self.advance();
+        }
+    }
+}
+
+pub fn safety_factor(robots: &[Robot]) -> u32 {
+    let mut tl = 0u32;
+    let mut tr = 0u32;
+    let mut bl = 0u32;
+    let mut br = 0u32;
+
+    let half_x = (WIDTH + 1) / 2 - 1;
+    let half_y = (HEIGHT + 1) / 2 - 1;
+
+    robots.iter().for_each(|robot| match robot.pos {
+        Vector2 { x, y } if x as u32 == half_x || y as u32 == half_y => (),
+        Vector2 { x, y } if (x as u32) < half_x && (y as u32) > half_y => tl += 1,
+        Vector2 { x, y } if (x as u32) > half_x && (y as u32) > half_y => tr += 1,
+        Vector2 { x, y } if (x as u32) < half_x && (y as u32) < half_y => bl += 1,
+        Vector2 { x, y } if (x as u32) > half_x && (y as u32) < half_y => br += 1,
+        _ => (),
+    });
+
+    tl * tr * bl * br
+}
+
+pub fn build_map(robots: &[Robot]) -> String {
+    let mut map: [[u32; WIDTH as usize]; HEIGHT as usize] = [[0; WIDTH as usize]; HEIGHT as usize];
+
+    robots.iter().for_each(|Robot { pos, .. }| {
+        map[pos.y as usize][pos.x as usize] += 1;
+    });
+
+    map.into_iter()
+        .map(|row| {
+            row.into_iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
+
+    let mut robots: Vec<_> = input
+        .lines()
+        .filter_map(|line| line.parse::<Robot>().ok())
+        .collect();
+
+    robots.iter_mut().for_each(|robot| robot.advance_n(100));
+
+    let safety = safety_factor(&robots);
+    println!("{safety}");
+
+    // let map = build_map(&robots);
+    // print!("{map}");
+
+    Ok(())
+}
